37ece4d374c8baa26bd26806e3180d8930d9f55c
byjdrk.27@gmail.com <byjdrk.27@gmail.com>
2025-09-24 10:07:49 -0300

    style(dashboad): index

--- dashboard/app/src/app/page.jsx
+++ dashboard/app/src/app/page.jsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Sidebar from "@/components/sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 p-6 ml-64 flex justify-center">
+        <div className="w-full max-w-4xl">
+          <h1 className="text-3xl font-bold text-center mb-4">Hello World</h1>
+          <p className="text-center">Aqui vai o conteúdo centralizado no espaço restante</p>
+        </div>
+      </main>
+    </div>
   );
 }
